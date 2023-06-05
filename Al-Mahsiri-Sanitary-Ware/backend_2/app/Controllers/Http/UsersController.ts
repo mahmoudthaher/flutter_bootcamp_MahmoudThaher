d9d167bbaaa6e63789174575fd2edc65ffd4c203
mthaher3@gmail.com <mthaher3@gmail.com>
2023-06-05 23:29:20 +0300

    forget password

--- Al-Mahsiri-Sanitary-Ware/backend_2/app/Controllers/Http/UsersController.ts
+++ Al-Mahsiri-Sanitary-Ware/backend_2/app/Controllers/Http/UsersController.ts
@@ -288,7 +288,7 @@ export default class UsersController {
     public async checkUserAndPhone(ctx: HttpContextContract) {
         var email = ctx.params.email;
         var phoneNumber = ctx.params.phoneNumber;
-        var result = User.query().where("email", email).andWhere("phone_number", phoneNumber);
+        var result = User.query().select("id").where("email", email).andWhere("phone_number", phoneNumber);
         return result;
     }
 
